96edde7d50b4d5351a2125ede2b5dde96079ce2d
Andrés Crespo <acrerosj@canariaseducacion.es>
2023-12-12 19:24:00 +0000

    send mail

--- mail/send.php
+++ mail/send.php
@@ -1,4 +1,6 @@
 <?php
+require_once '../vendor/autoload.php';
+require_once 'data.php';
 
 use PHPMailer\PHPMailer\PHPMailer;
 
@@ -6,12 +8,33 @@ $to = $_POST['to'];
 $subject = $_POST['subject'];
 $message = $_POST['message'];
 
+// Configuración del correo
 $mail = new PHPMailer();
 $mail->isSMTP();
-$mail->Host = 'smtp.gmail.com';
+$mail->Host = $host;
 $mail->SMTPAuth = true;
-$mail->Username = 
+$mail->Username = $user;
+$mail->Password = $pw;
+$mail->Port = 465;
+$mail->SMTPSecure = "ssl";
+$mail->setFrom($user, 'Andrés Profe');
 
-echo "<p>$to</p>";
-echo "<p>$subject</p>";
-echo "<p>$message</p>";
+// El mensaje del correo
+$mail->addAddress($to);
+$mail->Subject = $subject;
+$mail->Body = $message;
+
+if ($mail->send()) {
+  echo "<h1>Mensaje enviado correctamente</h1>";
+  echo "<p>Para: $to</p>";
+  echo "<p>Asunto: $subject</p>";
+  echo "<p>Mensaje: $message</p>";
+} else {
+  echo "<h1>No se ha podido enviar el mensaje</h1>";
+}
+
+
+// Cerrar la conexión
+$mail->smtpClose();
+
+
